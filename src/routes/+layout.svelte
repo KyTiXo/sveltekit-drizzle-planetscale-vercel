ec6c10f69aa8a87c5e69d625f994881df059d857
KyTiX <zkoolkyle@gmail.com>
2023-07-20 16:56:51 -0400

    tailwind, daisyui, kit seutp

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import '../app.css';
+
+	export let data: LayoutData;
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+	h1 {
+		@apply text-red-500;
+	}
+</style>
